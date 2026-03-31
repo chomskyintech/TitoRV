6d6588125177edd55436a88cef675142b9b33cb8
taha <tahaqadeerkhan@gmail.com>
2026-03-31 18:07:00 +0100

    changed the file in my local machine and now uploading the files again

---
+++ Tito.srcs/sources_1/new/main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 31.03.2026 16:45:56
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main(
+
+    );
+endmodule
